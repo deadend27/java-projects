f18862ba26e0e95aaa5bad7ad4509b14e7a8b091
deadend27 <emtiaz84960@gmail.com>
2024-01-18 16:07:09 +0600

    new files added

---
+++ Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args){
+        String university = "Port City International University";
+
+        System.out.println(university);
+
+        int num = 23;
+        System.out.println("int number is "+num);
+
+        char character='w';
+        System.out.println("character is " +character);
+
+        float FoatValue = 34.78f;
+        System.out.println("the float value is " +FoatValue);
+
+        boolean value = true;
+        System.out.println("the boolean value is " +value);
+    }
+}
---
+++ Variables2.java
@@ -0,0 +1,18 @@
+public class Variables2 {
+    public static void main(String[] args){
+        String firstName = "Emtiaz ";
+        String lastName = "Chowdhury";
+        String fullName = firstName + lastName;
+
+        System.out.println(fullName);
+
+        int x = 8;
+        int y = 4;
+
+        System.out.println("sume = " +(x+y));
+        System.out.println("sub = " +(x-y));
+        System.out.println("multi = " +(x*y));
+        System.out.println("div = " +(x/y));
+        System.out.println("mod = " +(x%y));
+    }
+}
